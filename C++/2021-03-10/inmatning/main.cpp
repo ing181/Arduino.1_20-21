7109bffa10992e82f9f5cd0bb213f36b301fd0d7
Ingemar Ceicer <ingcei0123@edu.lerum.se>
2021-03-10 09:18:09 +0100

    renamed

---
+++ C++/2021-03-10/inmatning/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// inmatning
+int main()
+{
+    int tal;
+    // cin är tangentbordet
+    // >> är inmatningsoperatorn
+    // tal är variabln som inmatningen hamnar i
+    cout << "Välj ett heltal ";
+    cin >> tal;
+    cout << "Du valde talet " << tal << endl;
+    return 0;
+}
---
+++ C++/2021-03-10/variabler/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// Om variabler
+int main()
+{
+    // int från "integer", heltal på engelska
+    // int talar om vad för slags data variablen kan innehålla
+    // tal är namnet på variabeln
+    // Använd alltid beskrivande namn
+    int alder;
+    // alder namnet på variabeln
+    // = tilldelningsoperatorn
+    // 5 data som tilldelas tal
+    // "Det som är till vänster om tilldelningsoperatorn tilldelas (får)
+    // värdet av det som är till höger"
+    alder = 5;
+    cout << alder << endl;
+    // värdet på en variabel kan variera
+    alder = 3;
+    cout << alder << endl;
+
+
+    return 0;
+}
