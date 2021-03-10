6a89fe77830c5c425505a49734d4977c83b33943
Ingemar Ceicer <ingcei0123@edu.lerum.se>
2021-03-10 08:48:31 +0100

    Lektion

---
+++ C++/2021-03-10/program1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // kommentar
+    // cout (skärmen)
+    // << (utmatningsoperator)
+    // "Hello world" (Texten som matas ut)
+    // << (utmatningsoperator)
+    // endl (ny rad)
+    // ; avslutar varje sats, "varje sak som ska göras"
+    cout << "Hejsan" << endl;
+
+    return 0;
+}
