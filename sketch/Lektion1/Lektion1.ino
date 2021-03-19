7f709ab4a77dc93ece32db491f0184a156f59677
Ingemar Ceicer <ingcei0123@edu.lerum.se>
2021-03-19 16:04:47 +0100

    Lektion 1!

---
+++ sketch/Lektion1/Lektion1.ino
@@ -0,0 +1,16 @@
+int pinLed = 8; // ordet "pinLed1" är nu samma sak som att
+                 // skriva siffran 8
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinLed,OUTPUT); // pin 8 ska användas för output
+ // pinMode(8,OUTPUT); // pin 8 ska användas för output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ digitalWrite(pinLed,HIGH); // Stömmen på
+  delay(1000); // Paus 1 sekund
+ digitalWrite(pinLed,LOW); // Stömmen på
+  delay(1000); // Paus 1 sekund
+}
